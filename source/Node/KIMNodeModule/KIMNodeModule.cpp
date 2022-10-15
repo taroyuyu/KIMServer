d6416428a843cc7c2ab8af9d6cc343852041ed56
taroyuyu <taroyuyu@icloud.com>
2022-10-15 23:38:30 +0800

    Implement the task queue function

---
+++ source/Node/KIMNodeModule/KIMNodeModule.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 凌宇 on 2022/10/15.
+//
+
+#include "KIMNodeModule.h"
---
+++ source/Node/KIMNodeModule/KIMNodeModule.h
@@ -0,0 +1,19 @@
+//
+// Created by 凌宇 on 2022/10/15.
+//
+
+#ifndef KAKAIMCLUSTER_KIMNODEMODULE_H
+#define KAKAIMCLUSTER_KIMNODEMODULE_H
+
+#include "../../Common/KIMModule.h"
+
+namespace kakaIM {
+    namespace node {
+        class KIMNodeModule : public common::KIMModule {
+
+        };
+    }
+}
+
+
+#endif //KAKAIMCLUSTER_KIMNODEMODULE_H
