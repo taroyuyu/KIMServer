81a99a5ed7724ae59b63d663e747bcc6c17ba493
taroyuyu <taroyuyu@icloud.com>
2022-10-16 19:22:38 +0800

    Use KIMNodeModule for refactoring

--- source/Node/Log/log.h
+++ source/Node/Log/log.h
@@ -4,15 +4,17 @@
 
 #ifndef KAKAIMCLUSTER_LOG_H
 #define KAKAIMCLUSTER_LOG_H
-const char * const NodeLogger = "Node";
-const char * const SingleChatModuleLogger = "Node.SingleChatModule";
-const char * const SessionModuleLogger = "Node.SessionModule";
-const char * const RosterModuleLogger = "Node.RosterModule";
-const char * const OnlineStateModuleLogger = "Node.OnlineStateModule";
-const char * const OfflineModuleLogger = "Node.OfflineModule";
-const char * const MessageSenderServiceModuleLogger = "Node.MessageSendServiceModule";
-const char * const GroupChatModuleLogger = "Node.GroupChatModule";
-const char * const ClusterModuleLogger = "Node.ClusterModule";
-const char * const AuthenticationModuleLogger = "Node.AuthenticationModule";
+namespace {
+    const char *const NodeLogger = "Node";
+    const char *const SingleChatModuleLogger = "Node.SingleChatModule";
+    const char *const SessionModuleLogger = "Node.SessionModule";
+    const char *const RosterModuleLogger = "Node.RosterModule";
+    const char *const OnlineStateModuleLogger = "Node.OnlineStateModule";
+    const char *const OfflineModuleLogger = "Node.OfflineModule";
+    const char *const MessageSenderServiceModuleLogger = "Node.MessageSendServiceModule";
+    const char *const GroupChatModuleLogger = "Node.GroupChatModule";
+    const char *const ClusterModuleLogger = "Node.ClusterModule";
+    const char *const AuthenticationModuleLogger = "Node.AuthenticationModule";
+}
 #endif //KAKAIMCLUSTER_LOG_H
 
--- source/President/Log/log.h
+++ source/President/Log/log.h
@@ -4,11 +4,13 @@
 
 #ifndef KAKAIMCLUSTER_LOG_H
 #define KAKAIMCLUSTER_LOG_H
-const char * const PresidentLogger = "Preisdent";
-const char * const ClusterManagerModuleLogger = "Preisdent.ClusterManagerModule";
-const char * const MessageIDGenerateModuleLogger = "Preisdent.MessageIDGenerateModule";
-const char * const NodeLoadBlanceModuleLogger = "Preisdent.NodeLoadBlanceModule";
-const char * const ServerRelayModuleLogger = "Preisdent.ServerRelayModule";
-const char * const UserStateManagerModuleLogger = "Preisdent.UserStateManagerModule";
+namespace {
+    const char *const PresidentLogger = "Preisdent";
+    const char *const ClusterManagerModuleLogger = "Preisdent.ClusterManagerModule";
+    const char *const MessageIDGenerateModuleLogger = "Preisdent.MessageIDGenerateModule";
+    const char *const NodeLoadBlanceModuleLogger = "Preisdent.NodeLoadBlanceModule";
+    const char *const ServerRelayModuleLogger = "Preisdent.ServerRelayModule";
+    const char *const UserStateManagerModuleLogger = "Preisdent.UserStateManagerModule";
+}
 #endif //KAKAIMCLUSTER_LOG_H
 
