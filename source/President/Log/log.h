77c23a571df06ee65318518f0e9ee03ef96da5fc
taroyuyu <taroyuyu@icloud.com>
2022-10-16 16:56:36 +0800

    Use KIMNodeModule for refactoring

--- source/President/Log/log.h
+++ source/President/Log/log.h
@@ -4,11 +4,11 @@
 
 #ifndef KAKAIMCLUSTER_LOG_H
 #define KAKAIMCLUSTER_LOG_H
-#define PresidentLogger "Preisdent"
-#define ClusterManagerModuleLogger "Preisdent.ClusterManagerModule"
-#define MessageIDGenerateModuleLogger "Preisdent.MessageIDGenerateModule"
-#define NodeLoadBlanceModuleLogger "Preisdent.NodeLoadBlanceModule"
-#define ServerRelayModuleLogger "Preisdent.ServerRelayModule"
-#define UserStateManagerModuleLogger "Preisdent.UserStateManagerModule"
+const char * const PresidentLogger = "Preisdent";
+const char * const ClusterManagerModuleLogger = "Preisdent.ClusterManagerModule";
+const char * const MessageIDGenerateModuleLogger = "Preisdent.MessageIDGenerateModule";
+const char * const NodeLoadBlanceModuleLogger = "Preisdent.NodeLoadBlanceModule";
+const char * const ServerRelayModuleLogger = "Preisdent.ServerRelayModule";
+const char * const UserStateManagerModuleLogger = "Preisdent.UserStateManagerModule";
 #endif //KAKAIMCLUSTER_LOG_H
 
