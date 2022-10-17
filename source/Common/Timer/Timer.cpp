858e84da1bed7e20ef3afafab55e40fed8596efc
taroyuyu <taroyuyu@icloud.com>
2022-10-17 09:33:58 +0800

    add Timer

--- source/Common/Timer/Timer.cpp
+++ source/Common/Timer/Timer.cpp
@@ -5,56 +5,6 @@
 #include <Common/Timer/Timer.h>
 
 namespace kakaIM {
-    template <class Rep, class Period>
-    void Timer::setTimeout(std::function<void()> task, const std::chrono::duration<Rep, Period> delay){
-        std::lock_guard<std::mutex> lock(this->mutex);
-        if (this->active){
-            return;
-        }
-
-        if (this->workThread.joinable()){
-            this->workThread.join();
-        }
-
-        this->active = true;
-        std::thread thread([=]() {
-            if (not this->active) {
-                return;
-            }
-            std::this_thread::sleep_for(delay);
-            if (not this->active) {
-                return;
-            }
-            task();
-            this->active = false;
-        });
-        this->workThread = std::move(thread);
-    }
-
-    template <class Rep, class Period>
-    void Timer::setInterval(std::function<void()> task, const std::chrono::duration<Rep, Period> interval) {
-        std::lock_guard<std::mutex> lock(this->mutex);
-        if (this->active){
-            return;
-        }
-
-        if (this->workThread.joinable()){
-            this->workThread.join();
-        }
-
-        this->active = true;
-        std::thread thread([=]() {
-            while (this->active) {
-                std::this_thread::sleep_for(interval);
-                if (not this->active) {
-                    break;
-                }
-                task();
-            }
-        });
-        this->workThread = std::move(thread);
-    }
-
     void Timer::stop() {
         std::lock_guard<std::mutex> lock(this->mutex);
         if (not this->active){
--- source/Common/Timer/Timer.h
+++ source/Common/Timer/Timer.h
@@ -10,6 +10,7 @@
 #include <atomic>
 #include <thread>
 #include <functional>
+
 namespace kakaIM {
     class Timer {
     private:
@@ -18,10 +19,57 @@ namespace kakaIM {
         std::thread workThread;
     public:
         ~Timer();
-        template <class Rep, class Period>
-        void setTimeout(std::function<void()> task, const std::chrono::duration<Rep, Period> delay);
-        template <class Rep, class Period>
-        void setInterval(std::function<void()> task, const std::chrono::duration<Rep, Period> interval);
+
+        template<class Rep, class Period>
+        void setTimeout(std::function<void()> task, const std::chrono::duration<Rep, Period> delay){
+            std::lock_guard<std::mutex> lock(this->mutex);
+            if (this->active){
+                return;
+            }
+
+            if (this->workThread.joinable()){
+                this->workThread.join();
+            }
+
+            this->active = true;
+            std::thread thread([=]() {
+                if (not this->active) {
+                    return;
+                }
+                std::this_thread::sleep_for(delay);
+                if (not this->active) {
+                    return;
+                }
+                task();
+                this->active = false;
+            });
+            this->workThread = std::move(thread);
+        }
+
+        template<class Rep, class Period>
+        void setInterval(std::function<void()> task, const std::chrono::duration<Rep, Period> interval) {
+            std::lock_guard<std::mutex> lock(this->mutex);
+            if (this->active) {
+                return;
+            }
+
+            if (this->workThread.joinable()) {
+                this->workThread.join();
+            }
+
+            this->active = true;
+            std::thread thread([=]() {
+                while (this->active) {
+                    std::this_thread::sleep_for(interval);
+                    if (not this->active) {
+                        break;
+                    }
+                    task();
+                }
+            });
+            this->workThread = std::move(thread);
+        }
+
         void stop();
     };
 }
